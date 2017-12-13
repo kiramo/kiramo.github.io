ceb9bf2878a418b39ca544c10a3892785a57217b
kiramo <32683504+kiramo@users.noreply.github.com>
2017-12-13 03:32:56 -0500

    Final Project - Nearly Complete
    Need Help with Last JS

--- morekira/js/main.js
+++ morekira/js/main.js
@@ -1,3 +1,54 @@
-$('a:hover').on('hover', function() {
-    $(#hover).addClass('color', '#7e667f');
-});
+
+
+  $('.clickme').click(function(e) {
+      e.preventDefault();
+
+      boxh = $('#popup').height();
+      windowh = $(window).height();
+
+      $('#popup').css('margin-top', windowh/2 - boxh/2);
+
+      $('#popup').fadeIn();
+  });
+  $('.clicktoclose').click(function(fadeout) {
+      $('#popup').fadeOut();
+  });
+
+
+  var offset = 250;
+
+  var duration = 300;
+
+  jQuery(window).scroll(function() {
+
+  if (jQuery(this).scrollTop() > offset) {
+
+  jQuery(‘.back-to-top’).fadeIn(duration);
+
+  } else {
+
+  jQuery(‘.back-to-top’).fadeOut(duration);
+
+  }
+
+  });
+
+
+
+  jQuery(‘.back-to-top’).click(function(e) {
+
+  jQuery(‘html, body’).animate({scrollTop: 0}, duration);
+
+  return false;
+
+  }
+
+  });
+
+  $('.one-time').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
