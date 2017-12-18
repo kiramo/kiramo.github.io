3ff1900f4ac04e3a3ed253f0a9c479184b2e47c7
kiramo <32683504+kiramo@users.noreply.github.com>
2017-12-17 21:56:39 -0500

    Final Project Troubleshoot

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-// Make it rain!
+// KONAMI!
+
+var kkeys = [];
+var konami = "38,38,40,40,37,39,37,39,66,65";
+
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+    $(document).unbind('keydown',arguments.callee);
+
+    $('body').css('background-color', '#db3f29');
+
+
+  }
+});
+
+
+  $('.clickme').click(function(e) {
+      e.preventDefault();
+
+      boxh = $('#popup').height();
+      windowh = $(window).height();
+
+      $('#popup').css('margin-top', windowh/2 - boxh/2);
+
+      $('#popup').fadeIn();
+  });
+  $('.clicktoclose').click(function(fadeout) {
+      $('#popup').fadeOut();
+  });
+
+
+  $('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+
+});
+
+
+
+  $('.hamburger').on('click',function() {
+    console.log('hello');
+    $('nav').toggleClass('menu-open');
+  });
